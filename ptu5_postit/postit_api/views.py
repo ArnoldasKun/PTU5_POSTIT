4331bfd8b82a752c2e0c4c72c73f10589506486b
Arnoldas Kundrotas <arnokund@gmail.com>
2022-11-25 12:12:37 +0200

    CRUD API for Post, comment model admin API

--- ptu5_postit/postit_api/views.py
+++ ptu5_postit/postit_api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics, permissions
+from rest_framework.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 from . import models, serializers
 
 
@@ -10,3 +12,55 @@ class PostList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class CommentList(generics.ListCreateAPIView):
+    #queryset = models.Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+    permissions_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        post = models.Post.objects.get(pk=self.kwargs['pk'])
+        serializer.save(user=self.request.user, post=post)
+
+    def get_queryset(self):
+        post = models.Post.objects.get(pk=self.kwargs['pk'])
+        return models.Comment.objects.filter(post=post)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Post.objects.all()
+    serializer_class = serializers.PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def delete(self, request, *args, **kwargs):
+        post = models.Post.objects.filter(pk=kwargs['pk'], user=self.request.user)
+        if post.exists():
+            return self.destroy(request, *args, **kwargs)
+        else:
+            raise ValidationError(_('You cannot delete posts not of your own'))
+
+    def put(self, request, *args, **kwargs):
+        post = models.Post.objects.filter(pk=kwargs['pk'], user=self.request.user)
+        if post.exists():
+            return self.update(request, *args, **kwargs)
+        else:
+            raise ValidationError(_("Can't change posts that are not yours!"))
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Post.objects.all()
+    serializer_class = serializers.PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def delete(self, request, *args, **kwargs):
+        comment = models.Comment.objects.filter(pk=kwargs['pk'], user=self.request.user)
+        if comment.exists():
+            return self.destroy(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        comment = models.Comment.objects.filter(pk=kwargs['pk'], user=self.request.user)
+        if comment.exists():
+            return self.update(request, *args, **kwargs)
+        else:
+            raise ValidationError(_(" You can't change comments not of your own!"))
